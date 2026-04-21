4299f9d5a0d82284c3202cc7b4cfa871547ec34e
Souad <souadkhalifeh620@gmail.com>
2026-04-21 22:37:36 +0300

    Created JSON Parser

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
 import path from "path";
 import logger from "./util/logger";
-import { readCSVFile } from "./util/parser";
+import { readCSVFile } from "./util/parsers/csvParser";
+import { readJSONFile } from "./util/parsers/jsonParser"; 
 
-const filePath = path.resolve(__dirname, "data/cake_orders.csv");
+const csvFile = path.resolve(__dirname, "data/cake_orders.csv");
+const jsonFile = path.resolve(__dirname, "data/book_orders.json");
 
 async function main(){
   try {
-      const data = await readCSVFile(filePath, true);
+    //Read CSV data
+      const orders = await readCSVFile(csvFile, true);
       //for each data row, log the row 
-      data.forEach((row)=>logger.info(row));
+      orders.forEach((row)=>logger.info(row));
+
+      //Read JSON data
+      const books = await readJSONFile(jsonFile);
+      books.forEach((name: string)=>logger.info(JSON.stringify(books)));
+
   }catch(err){
-    logger.error("Error in CSV Parse: %s", err);
+    logger.error("Error in Parse: %s", err);
   }
 
 }
---
+++ src/util/parsers/jsonParser.ts
@@ -0,0 +1,28 @@
+import { error } from 'console';
+import fs from 'fs'; 
+
+/*
+Reads a JSON file and returns its content as a JavaScript object
+@param filePath - the path to the JSON file
+@returns a promise that resolves to the parsed JavaScript object
+*/
+export async function readJSONFile(filePath:string): Promise<any> {
+    try {
+        const data = fs.readFileSync(filePath, 'utf8');
+        return JSON.parse(data);
+    } catch (error) {
+        throw new Error(`Error reading or parsing JSON file: ${error}`);
+    }
+}
+
+
+// Writes a JavaScript object to a JSON file
+export async function writeJSONFile(filePath:string, data: any): Promise<void> {
+    try {
+        const jsonData = JSON.stringify(data, null, 2);
+        fs.writeFileSync(filePath, jsonData, 'utf8');
+    } catch (error) {
+        throw new Error (`Error writing JSON file: ${error}`);
+
+    }
+}
